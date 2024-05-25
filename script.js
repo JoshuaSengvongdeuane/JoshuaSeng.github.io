c8c8a40db9a7332772d3457717a99b7045793370
Chewy <joshua.b.seng@gmail.com>
2024-05-25 02:16:25 -0700

    added project gallery

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function filterProjects(category) {
+  const projects = document.querySelectorAll('.pic');
+
+  projects.forEach(project => {
+    if (project.classList.contains(category) || category === 'all') {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+}
